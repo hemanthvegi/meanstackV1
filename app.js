d60f007849a3919c5c7e6b166b538d42872c1fe3
hemanthvegi92 <hemanthvegi92@gmail.com>
2019-09-01 22:05:06 +0530

    Code update for adding the frontend

--- app.js
+++ app.js
@@ -19,16 +19,20 @@ mongoose.connect("mongodb+srv://hem:"+  process.env.MONGO_ATLAS_PW +"@cluster0-x
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false}));
-app.use('/images',express.static(path.join('backend/images')))
-app.use((req, res, next) => {
-  res.setHeader("Access-Control-Allow-Origin", "*");
-  res.setHeader("Access-Control-Allow-Headers",
-   "Origin, X-Requested-with, Content-Type, Accept, Authorization");
-   res.setHeader("Access-Control-Allow-Methods",
-    "GET, POST, PATCH, DELETE, PUT, OPTIONS" )
-  next();
-})
+app.use('/images',express.static(path.join(__dirname, 'images')));
+app.use('/',express.static(path.join(__dirname,'angular')))
+// app.use((req, res, next) => {
+//   res.setHeader("Access-Control-Allow-Origin", "*");
+//   res.setHeader("Access-Control-Allow-Headers",
+//    "Origin, X-Requested-with, Content-Type, Accept, Authorization");
+//    res.setHeader("Access-Control-Allow-Methods",
+//     "GET, POST, PATCH, DELETE, PUT, OPTIONS" )
+//   next();
+// })
 
 app.use("/api/posts",postRoutes);
-app.use('/api/user', userRoutes)
+app.use('/api/user', userRoutes);
+app.use((req, res, next) => {
+  res.sendFile(path.join(__dirname, "angular", "index.html"));
+})
 module.exports = app;
